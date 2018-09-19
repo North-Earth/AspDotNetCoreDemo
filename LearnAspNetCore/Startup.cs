aa9a71bd23abe4649a4735e9b14b46af8cb12ebc
Revolt27 <kuckushckin.alex@yandex.ru>
2018-09-19 22:44:47 +0300

    Learn 2 | Основы работы метода Run

--- LearnAspNetCore/Startup.cs
+++ LearnAspNetCore/Startup.cs
@@ -11,8 +11,6 @@ namespace LearnAspNetCore
 {
     public class Startup
     {
-        int x = 2;
-
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
@@ -27,11 +25,20 @@ namespace LearnAspNetCore
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                x++;
-                await context.Response.WriteAsync($"x = {x}");
-            });
+            app.Run(Handle);
+        }
+
+        private async Task Handle(HttpContext context)
+        {
+            string host = context.Request.Host.Value;
+            string path = context.Request.Path;
+            string query = context.Request.QueryString.Value;
+
+            context.Response.ContentType = "text/html;charset=utf-8";
+
+            await context.Response.WriteAsync($"<h3>Хост: {host}</h3>" +
+                $"<h3>Пусть запроса: {path}</h3>" +
+                $"<h3>Параметры строки запроса: {query}</h3>");
         }
     }
 }
