0afb2a5d648b2917f4f421723eed46ce6caaaeed
Revolt27 <kuckushckin.alex@yandex.ru>
2018-10-09 20:06:17 +0300

    Learn 7 | Основы Web Api, REST запросы и работа с БД

---
+++ WebApiDemo/Models/UserContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebApiDemo.Models
+{
+    public class UsersContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public UsersContext(DbContextOptions<UsersContext> options)
+            : base(options) { }
+    }
+}
