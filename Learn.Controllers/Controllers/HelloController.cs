8c4743e3c7cb69d8db1dffa9f59ea662bbf63981
Revolt27 <kuckushckin.alex@yandex.ru>
2018-10-18 22:03:48 +0300

    Контроллеры | Начало работы с контроллерами

---
+++ Learn.Controllers/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Learn.Controllers.Controllers
+{
+    // [NonController] - Атрибут позволяет не рассматривать класс, как контроллер.
+    public class HelloController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // [NonAction] - Не рассматривает метод, как действие контроллера.
+        [ActionName("Welcome")] // Атрибут позволяет для метода задать другое имя действия.
+        public string Hello()
+        {
+            return "Hello ASP.NET";
+        }
+    }
+}
