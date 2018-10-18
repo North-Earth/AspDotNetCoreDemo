9644fe5acd611dcf36f3e12d70b70f025b927f72
Revolt27 <kuckushckin.alex@yandex.ru>
2018-10-18 22:29:55 +0300

    Контроллеры | Основы передачи данных

--- Learn.Controllers/Controllers/HelloController.cs
+++ Learn.Controllers/Controllers/HelloController.cs
@@ -7,17 +7,32 @@ namespace Learn.Controllers.Controllers
     // [NonController] - Атрибут позволяет не рассматривать класс, как контроллер.
     public class HelloController : Controller
     {
-        // GET: /<controller>/
-        public IActionResult Index()
-        {
-            return View();
-        }
-
         // [NonAction] - Не рассматривает метод, как действие контроллера.
         [ActionName("Welcome")] // Атрибут позволяет для метода задать другое имя действия.
-        public string Hello()
+        public string Hello(string name)
         {
-            return "Hello ASP.NET";
+            return name != null ? $"Hello, {name}!" : "Hello!";
+        }
+
+        public string Square(Geometry geometry) //Разрешено использование параметров по умолчанию.
+        {
+            return $"Площадь треугольника с основанием {geometry.Altitude} " +
+                $"и высотой {geometry.Height} равна {geometry.GetSquare()}";
+        }
+    }
+
+    public class Geometry
+    {
+        // Основание.
+        public int Altitude { get; set; }
+
+        // Высота.
+        public int Height { get; set; }
+
+        // Вычисление площади треугольника.
+        public double GetSquare()
+        {
+            return Altitude * Height / 2;
         }
     }
 }
