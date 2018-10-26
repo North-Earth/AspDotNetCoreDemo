987a2ac204cb76d00f1c07c8567f68d08d639cec
Revolt27 <kuckushckin.alex@yandex.ru>
2018-10-26 23:36:17 +0300

    Контроллеры | Контекст контроллера

---
+++ Learn.Controllers/Controllers/ContextController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn.Controllers.Controllers
+{
+    public class ContextController : Controller
+    {
+        // Responce - управление ответом на запрос.
+        public void Index()
+        {
+            Response.StatusCode = 404;
+            Response.ContentType = "application/json";
+            Response.WriteAsync("Resource not found");
+        }
+
+        // Получаем данные о запросе.
+        public void GetRequest()
+        {
+            string table = "";
+            foreach (var header in Request.Headers)
+            {
+                table += $"<tr><td>{header.Key}</td><td>{header.Value}</td></tr>";
+            }
+            Response.WriteAsync(String.Format("<table>{0}</table>", table));
+
+            // Получение информации из определённых заголовков.
+            //string userAgent = Request.Headers["User-Agent"].ToString();
+            //string referer = Request.Headers["Referer"].ToString();
+        }
+    }
+}
