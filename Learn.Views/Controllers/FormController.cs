9a2ca571d87c4deabddca87f63bb64bad9e32209
Revolt27 <kuckushckin.alex@yandex.ru>
2018-11-05 23:26:36 +0300

    Представления | Работа с формами

---
+++ Learn.Views/Controllers/FormController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn.Views.Controllers
+{
+    public class FormController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string login, string password, int age, string comment, bool isMarried, string color, string car, string[] cars)
+        {
+            string content = $"Login: {login}  " +
+                $" Password: {password}  " +
+                $" Age: {age}  " +
+                $"Comment: {comment} " +
+                $"isMarried: {isMarried} " +
+                $"color: {color} " +
+                $"car: {car} ";
+
+            foreach (string c in cars)
+            {
+                content += c;
+                content += "; ";
+            }
+            return Content(content);
+        }
+    }
+}
