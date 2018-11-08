34e65b1d6bba6f601055e4c90caeb5494e3edb6a
Revolt27 <kuckushckin.alex@yandex.ru>
2018-11-08 23:38:41 +0300

    Маршрутизация | Основы маршрутов в MVC

---
+++ Learn.Routing/Controllers/RoutingController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn.Routing.Controllers
+{
+    public class RoutingController : Controller
+    {
+        /// <summary>
+        /// Получение информации о маршруте.
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            var controller = RouteData.Values["controller"].ToString();
+            var action = RouteData.Values["action"].ToString();
+            return Content($"controller: {controller} | action: {action}");
+        }
+    }
+}
