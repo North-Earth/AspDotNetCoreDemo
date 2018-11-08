767056f550fb9644577271f8ade1ca93f089face
Revolt27 <kuckushckin.alex@yandex.ru>
2018-11-08 23:55:42 +0300

    Маршрутизация | Атрибуты маршрутизации

--- Learn.Routing/Controllers/RoutingController.cs
+++ Learn.Routing/Controllers/RoutingController.cs
@@ -6,17 +6,40 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Learn.Routing.Controllers
 {
+    [Route("main")] //Префикс для всего контроллера.
+    [Route("[controller]")] // Возможно указывать множественные маршруты.
     public class RoutingController : Controller
     {
         /// <summary>
         /// Получение информации о маршруте.
         /// </summary>
         /// <returns></returns>
+        [Route("infopage")] // Явное указание маршрута 
+        [Route("[action]")] // Второй маршрут.
         public IActionResult Index()
         {
             var controller = RouteData.Values["controller"].ToString();
             var action = RouteData.Values["action"].ToString();
             return Content($"controller: {controller} | action: {action}");
         }
+
+        [Route("{id:int}/{name:maxlength(10)}")]
+        public IActionResult Test(int id, string name)
+        {
+            return Content($" id={id} | name={name}");
+        }
+
+        // Опеределение пути через параметры.
+        // /main/routing/exampleresult
+        [Route("[controller]/[action]")]
+        //[Route("[controller]")]
+        //[Route("[action]")]
+        //[Route("[controller]/[action]/{id?}")]
+        public IActionResult ExampleResult()
+        {
+            return Content("Test text");
+        }
+
+
     }
 }
