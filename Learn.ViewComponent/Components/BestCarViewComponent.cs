d49f8be323f01abf7191778fa17eb926cea0aecf
Revolt27 <kuckushckin.alex@yandex.ru>
2018-11-13 23:52:49 +0300

    View Component | Определение компонента представлений

---
+++ Learn.ViewComponent/Components/BestCarViewComponent.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Learn.ViewComponent.Models;
+
+namespace Learn.ViewComponent.Components
+{
+    /*
+     * Компонент должен содержать суффикс ViewComponent 
+     * или быть наследованым от ViewComponent.
+     * Так же можно использовать атрибут [ViewComponent], 
+     * в таком случае суффикс и наследование не требуется.
+     */
+    public class BestCarViewComponent 
+    {
+        private List<Car> cars;
+
+        public BestCarViewComponent()
+        {
+            cars = new List<Car>
+            {
+                new Car {Model = "Tesla Model S", Price = 100900 },
+                new Car {Model = "Tesla Model X", Price = 137700 }
+            };
+        }
+
+        public string Invoke()
+        {
+            var item = cars.OrderByDescending(x => x.Price).Take(1).FirstOrDefault();
+            return $"Самая дорогая модель: {item.Model}  Цена: {item.Price}";
+        }
+    }
+}
