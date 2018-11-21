bcc3f2bc72a398ea838e17e94e01966880fda984
Revolt27 <kuckushckin.alex@yandex.ru>
2018-11-21 23:11:30 +0300

    View Component | Асинхронные операции

---
+++ Learn.ViewComponent/Components/Header.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace Learn.ViewComponent.Components
+{
+    public class Header : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            string htmlContent = String.Empty;
+
+            using (FileStream fileStream = new FileStream("Files/header.html", FileMode.Open))
+            {
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    htmlContent = await reader.ReadToEndAsync();
+                }
+            }
+
+            return new HtmlContentViewComponentResult(new HtmlString(htmlContent));
+        }
+    }
+}
