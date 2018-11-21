926791fee7e4c008bcf2c3cd5c0d3b93bbfdebfc
Revolt27 <kuckushckin.alex@yandex.ru>
2018-11-21 23:40:24 +0300

    View Component | Закрепление выполнения Асинхронных операций

---
+++ Learn.ViewComponent/Components/RandomNumbersCollection.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Learn.ViewComponent.Components
+{
+    public class RandomNumbersCollection : Microsoft.AspNetCore.Mvc.ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var collection = new List<int>();
+            var random = new Random();
+
+            for (int i = 0; i < 200; i++)
+            {
+                collection.Add(random.Next(int.MaxValue));
+                await Task.Delay(100);
+            }
+
+            return View("", collection);
+        }
+    }
+}
