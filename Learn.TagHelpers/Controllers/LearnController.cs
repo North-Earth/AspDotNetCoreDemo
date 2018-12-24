31bc1873eaedafa9421c123d8902d91b66431b4d
Revolt27 <kuckushckin.alex@yandex.ru>
2018-12-24 23:45:48 +0300

    Tag-хелперы | Tag-хелперы форм

--- Learn.TagHelpers/Controllers/LearnController.cs
+++ Learn.TagHelpers/Controllers/LearnController.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Learn.TagHelpers.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Learn.TagHelpers.Controllers
 {
@@ -16,6 +16,26 @@ namespace Learn.TagHelpers.Controllers
         public string Create(int id, int age, string name)
         {
             return $"id:{id} age:{age} name:{name}";
+        }
+
+        private IEnumerable<Company> companies = new List<Company>
+        {
+            new Company { Id = 1, Name = "Apple" },
+            new Company { Id = 2, Name = "Samsung" },
+            new Company { Id=3, Name="Microsoft" }
+        };
+
+        public IActionResult CreatePhone()
+        {
+            ViewBag.Companies = new SelectList(companies, "Id", "Name");
+            return View();
+        }
+
+        [HttpPost]
+        public string CreatePhone(Phone phone)
+        {
+            Company company = companies.FirstOrDefault(c => c.Id == phone.CompanyId);
+            return $"Добавлен новый элемент: {phone.Name} ({company?.Name})";
         }
     }
 }
---
+++ Learn.TagHelpers/Models/Phone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learn.TagHelpers.Models
+{
+    public class Phone
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+
+        public int CompanyId { get; set; }
+        public Company Company { get; set; }
+    }
+}
